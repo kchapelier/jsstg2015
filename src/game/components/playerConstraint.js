e557c1611555d94546d3b86124a5698e8db89414
kchapelier <kevin.chapelier@gmail.com>
2015-02-13 23:36:20 +0100

    update playerConstraint and playerShotRender to play nicely with centered anchor

--- src/game/components/playerConstraint.js
+++ src/game/components/playerConstraint.js
@@ -5,7 +5,7 @@ var renderer = require('../renderer');
 module.exports = {
     postUpdate: function (element) {
         //limit the player movement to the screen
-        element.x = Math.max(0, Math.min(renderer.screenWidth - 32, element.x));
-        element.y = Math.max(0, Math.min(renderer.screenHeight - 32, element.y));
+        element.x = Math.max(8, Math.min(renderer.screenWidth - 8, element.x));
+        element.y = Math.max(8, Math.min(renderer.screenHeight - 8, element.y));
     }
 };
--- src/game/components/playerShotRender.js
+++ src/game/components/playerShotRender.js
@@ -8,9 +8,10 @@ var texture = textureCollection.get('player-bullet');
 module.exports = {
     initialize: function (element) {
         element.sprite = new PIXI.Sprite(texture);
+        element.sprite.anchor = new PIXI.Point(0.5, 0.5);
     },
     render: function (element) {
-        element.sprite.x = Math.round(element.x) + (32 - element.sprite.width) / 2;
-        element.sprite.y = Math.round(element.y);
+        element.sprite.x = element.x;
+        element.sprite.y = element.y;
     }
 };
